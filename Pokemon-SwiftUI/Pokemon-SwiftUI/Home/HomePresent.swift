b2315f581f88d00330a95b49dcfff6995fd913ef
Paulo Roberto <paulora999@gmail.com>
2023-02-15 22:09:36 -0300

    Initial Config

---
+++ Pokemon-SwiftUI/Pokemon-SwiftUI/Home/HomePresent.swift
@@ -0,0 +1,8 @@
+//
+//  HomePresent.swift
+//  Pokemon-SwiftUI
+//
+//  Created by Paulo Roberto on 15/02/23.
+//
+
+import Foundation
---
+++ Pokemon-SwiftUI/Pokemon-SwiftUI/Home/HomeRouter.swift
@@ -0,0 +1,12 @@
+//
+//  HomeRouter.swift
+//  Pokemon-SwiftUI
+//
+//  Created by Paulo Roberto on 15/02/23.
+//
+
+import Foundation
+
+final class HomeRouter: BaseRouter {
+    
+}
